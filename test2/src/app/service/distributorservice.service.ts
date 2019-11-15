39d904596b06ee05091db50f2c78e4a8f1331cba
tharinduvindula <tharinduvindula@gmail.com>
2019-11-16 01:27:23 +0530

    add distributor service

--- test2/src/app/service/distributorservice.service.ts
+++ test2/src/app/service/distributorservice.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { EthcontractService } from 'app/web3-servise/ethcontract.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ export class DistributorserviceService {
   constructor(private web3: EthcontractService) {
   }
 
-  async getAdminCount() {
-    await this.web3.getAdminvalue().getAdminCount().call((er: any, ev: number) => {
+  async getDistributorCount() {
+    await this.web3.getDistributorvalue().getDistributorCount().call((er: any, ev: number) => {
       if (er == null) {
         this.result = ev;
       } else {
@@ -20,14 +21,15 @@ export class DistributorserviceService {
     return this.result;
   }
 
-  insertAdmin(email, name) {
-    this.web3.setAdminvalue().then((deployed: { insertAdmin: (arg0: string, arg1: string, arg2: { from: string; }) => void; }) => {
-      deployed.insertAdmin(email, name, { from: this.web3.curentaccount });
+  insertDistributor(email, name) {
+    // tslint:disable-next-line: max-line-length
+    this.web3.setDistributorvalue().then((deployed: { insertDistributor: (arg0: string, arg1: string, arg2: { from: string; }) => void; }) => {
+      deployed.insertDistributor(email, name, { from: this.web3.curentaccount });
     });
   }
 
   async getDistributori(index) {
-    await this.web3.getAdminvalue().getAdmini(index).call((er: any, ev: any) => {
+    await this.web3.getDistributorvalue().getDistributori(index).call((er: any, ev: any) => {
       if (er == null) {
         this.result = ev;
       } else {
